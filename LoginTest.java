c7f67d579fb86c1bd3a4b21200c58d698aa2bda4
ShehabElDeenTawfik <shohob2009@gmail.com>
2025-11-10 00:31:02 +0200

    Add files via upload
    Add TC002: automation script for invalid login

--- LoginTest.java
+++ LoginTest.java
@@ -2,6 +2,7 @@ package Tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -21,6 +22,20 @@ ChromeDriver driver = new ChromeDriver();
     driver.findElement(By.id("password")).sendKeys("secret_sauce");
     driver.findElement(By.id("login-button")).click();
 
+    String actual = driver.findElement(By.className("app_logo")).getText();
+     Assert.assertEquals(actual,"Swag Labs");
+ }
+
+ @Test
+ void TC002()
+ {
+     driver.findElement(By.id("user-name")).sendKeys("invalid_user");
+     driver.findElement(By.id("password")).sendKeys("wrong_pass");
+     driver.findElement(By.id("login-button")).click();
+
+     String ActualError = driver.findElement(By.cssSelector("h3[data-test=\"error\"]")).getText();
+     String ExpetedError = "Epic sadface: Username and password do not match any user in this service";
+     Assert.assertEquals(ActualError,ExpetedError,"Error message mismatch!");
  }
 @AfterTest
     void  close()
